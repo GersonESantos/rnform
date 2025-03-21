083261c0c6571dbe25bdeb095840ef6a35fda823
GersonESS <gebhsantos@gmail.com>
2025-03-21 16:56:49 -0300

    a

--- 01rn/src/screems/FormStepOne/index.tsx
+++ 01rn/src/screems/FormStepOne/index.tsx
@@ -1,11 +1,13 @@
 import{ Text, View} from 'react-native';
 import { styles } from './styles';
+import { Input } from '../components/input';
 export function FormStepOne() {
   return (
     <View style={styles.container}>
       <Text style={styles.title}>
         Criar sua conta
       </Text>
+      <Input icon="user"/>
     </View>
   );
 }
---
+++ 01rn/src/screems/components/input/index.tsx
@@ -0,0 +1,18 @@
+import{ TextInput, View } from 'react-native';
+import { Feather } from '@expo/vector-icons';
+import { styles } from './styles';
+
+type Props = {
+    icon: keyof typeof Feather.glyphMap;
+    placeholder: string;
+}
+
+export function Input({ icon }: Props) {
+    return (
+       <View>
+        <Feather name={icon} size={24} color="red"/>
+       <TextInput {style=styles.control}
+       />
+       </View>
+    );
+}
