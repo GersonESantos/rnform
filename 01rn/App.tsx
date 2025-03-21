507136f7740565b237ceb0bb950d262c85e85b86
GersonESS <gebhsantos@gmail.com>
2025-03-21 16:10:06 -0300

    Update App.tsx

--- 01rn/App.tsx
+++ 01rn/App.tsx
@@ -1,20 +1,11 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import { FormStepOne } from './src/screems/FormStepOne';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <FormStepOne />
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+
