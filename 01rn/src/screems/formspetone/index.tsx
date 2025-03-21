022be62299d61bac7aa52446bd958778aa43bc2c
GersonESS <gebhsantos@gmail.com>
2025-03-21 16:00:46 -0300

    a

---
+++ 01rn/src/screems/formspetone/index.tsx
@@ -0,0 +1,11 @@
+import{ Text, View} from 'react-native';
+import { styles } from './style';
+export function FormStepOne() {
+  return (
+    <View style={styles.container}>
+      <Text style={styles.title}>
+        Criar sua conta
+      </Text>
+    </View>
+  );
+}
